357647866a67398de0e8a46ea3ec47555ebf2ddc
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-09-30 19:45:30 -0400

    adds some styling and behaviour to the buttons

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
 
-	var buttonsInfo = [
-		{ title: 'RESUME', href:'#resume' },
-		{ title: 'PROJECTS', href:'#projects' },
-		{ title: 'ABOUT' , href: '#about'}];
+	(function(){
+		var buttonsInfo = [
+			{ title: 'RESUME', href:'#resume' },
+			{ title: 'PROJECTS', href:'#projects' },
+			{ title: 'ABOUT' , href: '#about'}];
 
-	var $buttonsArray = buttonsInfo.map(function(obj) {
-		return $("<button class='btn' href="+ obj.href +">" + obj.title + "</title>");
-	});
+		//create the buttons and append them to the #buttons dom Element
+		var $buttonsArray = buttonsInfo.map(function(obj) {
+			return $("<button class='btn' href="+ obj.href +">" + obj.title + "</title>");
+		});
 
-	$('#buttons').append($buttonsArray); 
+		$('#buttons').append($buttonsArray); 
+
+		//animate the buttons on mouseenter and leave
+
+		$('.btn').on("mouseenter", function(){
+			$(this).animate({"width" : "450px"}, 200)
+		}).on("mouseleave", function(){
+			$(this).animate({"width" : "400px"}, 300)
+		});
+
+	})();
+
 });
