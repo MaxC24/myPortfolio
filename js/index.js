165a4037e80aaf99cf98a168f09d8f4026dedc77
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-10-03 00:33:10 -0400

    reordering components and creates the projects component

--- js/index.js
+++ js/index.js
@@ -1,20 +1,15 @@
 $(document).ready(function(){
 
 	(function(){
+
+		//create the buttons, append them to the dom and animate them onclick:
+
 		var buttonsInfo = [
 			{ title: 'RESUME'},
 			{ title: 'PROJECTS' },
-			{ title: 'ABOUT'}];
+			{ title: 'ABOUT'}
+		];
 
-		// shows the resume in a lightbox
-		var $resume = $('<div class="lightbox"><Object data="./img/resume.pdf" type="application/pdf" width="70%" height="100%""></Object></div>');
-		$resume.on('click', function(e){
-			$(this).animate({opacity: 0}, 500, function(){
-				$(this).detach();
-			})
-		});
-
-		//create the buttons and append them to the #buttons dom Element
 		var $buttonsArray = buttonsInfo.map(function(obj) {
 			return $("<button class='btn'>" + obj.title + "</title>");
 		});
@@ -23,11 +18,23 @@ $(document).ready(function(){
 
 		//animate the buttons on mouseenter and leave
 
-		$('.btn').on("mouseenter", function(){
-			$(this).animate({"width" : "450px"}, 200)
-		}).on("mouseleave", function(){
-			$(this).animate({"width" : "400px"}, 300)
+		$('.btn').on({
+			mouseenter: function(){
+				$(this).animate({"width" : "450px"}, 200);
+			},
+			mouseleave: function(){
+				$(this).animate({"width": "400px"}, 300);
+			}
 		});
+
+		// create the resume dom Element and attach it to a display none lightbox that will fade/in at resume btn onclick
+		var $resume = $('<div class="lightbox"><Object data="./img/resume.pdf" type="application/pdf" width="70%" height="100%""></Object></div>');
+		$resume.on('click', function(e){
+			$(this).animate({opacity: 0}, 500, function(){
+				$(this).detach();
+			});
+		});
+;
 
 		//action when clicking on the buttons
 		$('.btn').click(function(){
