a4edf512a85b06a08337eb06782801e28bc96107
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-10-03 01:25:05 -0400

    creates helper functions addthe project component to the index.js file and reorder code and adds comments

--- js/index.js
+++ js/index.js
@@ -27,30 +27,74 @@ $(document).ready(function(){
 			}
 		});
 
+		//RESUME BUTTON
 		// create the resume dom Element and attach it to a display none lightbox that will fade/in at resume btn onclick
-		var $resume = $('<div class="lightbox"><Object data="./img/resume.pdf" type="application/pdf" width="70%" height="100%""></Object></div>');
-		$resume.on('click', function(e){
-			$(this).animate({opacity: 0}, 500, function(){
-				$(this).detach();
-			});
-		});
-;
+		var $resume = '<Object data="./img/resume.pdf" type="application/pdf" width="70%" height="100%""></Object>';
+		$resume = addToLightBox($resume);
+		addOnClickEvent($resume);
 
+		//PROJECTS BUTTON
+		//create the projects lightbox component and add onlickevent.
+
+		var projects = [{
+			name: "Pop Writer",
+			url: "http://www.pop-writer.com",
+			img: "img/popwriter.png"
+		},{
+			name: "Movies App",
+			url: "www.themoviesapp.xyz",
+			img: "img/movies.png"
+		},{
+			name: "Twitter Heat Map",
+			url: "https://github.com/Sushi1985/Twitter-Heat-Map",
+			img: "img/twitter.png"
+		},{
+			name: "Imp.nyc",
+			url: "www.imp.nyc",
+			img: "img/imp.png"
+		}];
+
+		var $projects = projects.reduce(function(a, b){
+			return a + "<div class='project'><a href=" + b.url + "><img src=" + b.img + " /></a><div class='project-title'>" + b.name + "</div></div>";
+		}, '');
+
+		$projects = addToLightBox($projects);
+		addOnClickEvent($projects);
+
+		//BUTTONS onclick actions
 		//action when clicking on the buttons
 		$('.btn').click(function(){
 			switch($(this)[0].innerHTML) {
 				case 'RESUME':
-					$("#screen").append($resume);
-					$resume.animate({opacity: 1}, 500);
+					appendToScreenAndAnimate($resume);
 					break;
 				case 'PROJECTS':
-					console.log('projects');
+					appendToScreenAndAnimate($projects);
 					break;
 				case 'ABOUT':
 					console.log('About');
 					break;
 			}
 		});
+
+		//HELPER FUNCTIONS
+		
+		function addToLightBox(domElement){
+			return  $('<div class="lightbox">' + domElement + '</div>');
+		}
+
+		function addOnClickEvent(domElement) {
+			domElement.on('click', function(e){
+				$(this).animate({opacity: 0}, 500, function(){
+					$(this).detach();
+				});
+			});
+		}
+
+		function appendToScreenAndAnimate(domElement) {
+			$('#screen').append(domElement);
+			domElement.animate({opacity: 1}, 500);
+		}
 
 	})();
 
